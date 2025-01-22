9ca36da90ed1f518db554edeb79e26951a6fffa2
Javier Sin Pelayo <843442@unizar.es>
2025-01-22 16:57:09 -0500

    Middleware: admin routes protected
    Now the middleware protects the admin routes for the non admin users.
    
    IMPORTANT: now it's only handled by the cookies, but later it'll be implemented with the native Supabase auth API calls for security concerns

--- fit-connect/src/middleware.ts
+++ fit-connect/src/middleware.ts
@@ -1,8 +1,30 @@
-import { type NextRequest } from 'next/server';
-import { updateSession } from '@/utils/supabase/middleware';
+import { type NextRequest, NextResponse } from 'next/server';
+import { updateSession } from './app/utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  // 1) Call the updateSession function to handle Supabase auth checks
+  let supabaseResponse = await updateSession(request);
+
+  // 2) Extract the role from cookies
+  const role = supabaseResponse.cookies.get('role')?.value || 'user';
+
+  // 3) Check if user is visiting an admin page
+  if (request.nextUrl.pathname.startsWith('/home/admin') && role !== 'admin') {
+    // 4) Redirect them to /error
+    const url = request.nextUrl.clone();
+    url.pathname = '/error';
+    const redirectResponse = NextResponse.redirect(url);
+
+    // Now it's necessary to copy the cookies from supabaseResponse to ensure we don't lose session data
+    const originalCookies = supabaseResponse.cookies.getAll();
+    for (const cookie of originalCookies) redirectResponse.cookies.set(cookie.name, cookie.value);
+
+    return redirectResponse;
+  }
+
+  // 5) Otherwise, allow the user to continue
+  return supabaseResponse;
+  // return await updateSession(request);
 }
 
 export const config = {
@@ -12,11 +34,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * - login (login page)
-     * - signup (signup page)
-     * - api (API routes)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|login|signup|passwordRecovery|reset-password|api).*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
